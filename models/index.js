0dace2b5a8234a8b10ba1145ae53e2cf1bfca567
Todd Malone <toddomalone@gmail.com>
2021-03-24 20:41:12 -0500

    added relationship for User-Article

--- models/index.js
+++ models/index.js
@@ -1,4 +1,7 @@
 const User = require('./User');
 const Article = require('./Article');
 
+User.hasMany(Article,{foreignKey:'user_id'})
+Article.belongsTo(User,{foreignKey:'user_id'})
+
 module.exports = { User,Article };
