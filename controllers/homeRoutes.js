db3a74700285435114b699249778732f92700060
Todd Malone <toddomalone@gmail.com>
2021-03-26 22:43:08 -0500

    added dashboard to display only user's posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,6 +14,27 @@ router.get('/', withAuth, async (req, res) => {
     res.render('homepage', {
       articleArray,
       logged_in: req.session.logged_in,
+      title:'Blogfish'
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const articles = await Article.findAll({
+      include:[{model:User,attributes:['name']}],
+      order:[['created_at','ASC']],
+      where:{user_id:req.session.user_id}
+    });
+
+    const articleArray= articles.map((article) => article.get({ plain: true }));
+
+    res.render('dashboard', {
+      articleArray,
+      logged_in: req.session.logged_in,
+      title:'Dashboard'
     });
   } catch (err) {
     res.status(500).json(err);
